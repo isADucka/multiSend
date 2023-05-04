b78f1768c49bb549d3cc97f64c4f58af833b7add
isADucka <2867025942@qq.com>
2023-05-04 22:15:04 +0800

    返回结构

---
+++ multisend-common/src/main/java/com/yyyy/multisend/common/models/Models.java
@@ -0,0 +1,12 @@
+package com.yyyy.multisend.common.models;
+
+import java.io.Serializable;
+
+/**
+ * @author isADuckA
+ * @Date 2023/4/23 22:14
+ * 做到统一的返回结构
+ */
+
+public class Models implements Serializable {
+}
