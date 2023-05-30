427ee4232a977e17f897a44d35f8917df600e1f5
isADucka <2867025942@qq.com>
2023-05-30 20:01:26 +0800

    启动类

--- multisend-web/src/main/java/com/yyyy/multisend/web/MultisendWebApplication.java
+++ multisend-web/src/main/java/com/yyyy/multisend/web/MultisendWebApplication.java
@@ -1,5 +1,6 @@
 package com.yyyy.multisend.web;
 
+import com.dtp.core.spring.EnableDynamicTp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("com.yyyy.multisend.dao.Mapper")
 @EntityScan("com.yyyy.multisend.common.povo.po")
+@EnableDynamicTp
 public class MultisendWebApplication {
 
     public static void main(String[] args) {
