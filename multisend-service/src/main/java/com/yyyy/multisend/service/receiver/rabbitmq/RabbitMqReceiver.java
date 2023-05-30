0e29063fc07102036a528685256a5212a2dbb0c2
isADucka <2867025942@qq.com>
2023-05-30 20:00:03 +0800

    消息队列

--- multisend-service/src/main/java/com/yyyy/multisend/service/receiver/rabbitmq/RabbitMqReceiver.java
+++ multisend-service/src/main/java/com/yyyy/multisend/service/receiver/rabbitmq/RabbitMqReceiver.java
@@ -2,20 +2,16 @@ package com.yyyy.multisend.service.receiver.rabbitmq;
 
 
 import com.alibaba.fastjson.JSONObject;
-import com.yyyy.multisend.common.models.Models;
 import com.yyyy.multisend.common.ssm.MsgTask;
 import com.yyyy.multisend.service.receiver.Consumer;
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import sun.reflect.misc.ReflectUtil;
 
-import java.lang.reflect.Field;
+
 
 /**
  * @author isADuckA
@@ -29,7 +25,6 @@ public class RabbitMqReceiver {
     private String queueName;
 
 
-
     @Autowired
     private Consumer consumer;
 
@@ -41,6 +36,7 @@ public class RabbitMqReceiver {
     @RabbitListener(queues = "${multisend.rabbitmq.queueName}")
     public void comsume(Message message) {
 
+
         byte[] body = message.getBody();
         MsgTask msgTask = JSONObject.parseObject(body, MsgTask.class);
 //        Models models = msgTask.getModels();
@@ -49,6 +45,7 @@ public class RabbitMqReceiver {
         //调用消费的service来处理从队列中拿出来的消息
         //这里调用service是因为如果有多个中间件的话就会很多重复，所以调用一个统一的去执行
 
+//        System.out.println("现在在获取消费"+msgTask);
         consumer.comsumerMsg(msgTask);
 
 
