2bb2f163bddea0b34f4a878b2e1c421f8598fabc
isADucka <2867025942@qq.com>
2023-05-30 20:01:12 +0800

    上传人群文件

--- multisend-web/src/main/java/com/yyyy/multisend/web/controller/ControModelsController.java
+++ multisend-web/src/main/java/com/yyyy/multisend/web/controller/ControModelsController.java
@@ -1,11 +1,19 @@
 package com.yyyy.multisend.web.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.base.Throwables;
 import com.yyyy.multisend.common.povo.vo.Result;
 import com.yyyy.multisend.common.povo.po.MessageModel;
 import com.yyyy.multisend.service.service.ControModelsService;
 import com.yyyy.multisend.web.annocation.MultisendAspect;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 import java.util.List;
 
@@ -17,15 +25,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/contro")
 @MultisendAspect
+@Slf4j
 public class ControModelsController {
 
+
+    @Value("${multisend.cron.path}")
+    private String dataPath;
     @Autowired
     private ControModelsService controModelsService;
 
     @GetMapping("/save")
     public Result SaveMsgModel(@RequestBody MessageModel messageModel){
-        controModelsService.saveModel(messageModel);
-        return null;
+       return controModelsService.saveModel(messageModel);
+
     }
 
 
@@ -34,6 +46,33 @@ public class ControModelsController {
         System.out.println(modelId);
         //删除模板
         return controModelsService.deleteModel(modelId);
+
+    }
+
+    /**
+     * 上传目标人群文件
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("upload")
+    public Result saveFile(@RequestParam("file")MultipartFile file) throws IOException {
+        String filePath =dataPath+file.getOriginalFilename();
+        try {
+            File localFile = new File(filePath);
+            if (!localFile.exists()) {
+                localFile.mkdirs();
+            }
+            // 注意一点的是，没有用绝对路径会报错
+
+            file.transferTo(localFile.getAbsoluteFile());
+            System.out.println(file.getResource());
+        } catch (Exception e) {
+            log.error("MessageTemplateController#upload fail! e:{},params{}", Throwables.getStackTraceAsString(e), JSON.toJSONString("找不到指定路径"));
+        }
+
+        return new Result(filePath);
+
 
     }
 
